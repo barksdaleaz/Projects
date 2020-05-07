a6ec8501193ce2c4e017eee7f31f2211a348313b
barksdaleaz <59379235+barksdaleaz@users.noreply.github.com>
2020-05-07 18:55:09 -0500

    prevents alpha/no input, and limits digits to 100

--- Numbers/findpi.py
+++ Numbers/findpi.py
@@ -1,8 +1,22 @@
 import math
 
+def calculate(roundint):
+    roundval = round(math.pi, roundint)
+    return roundval
+
 roundto = input("Enter the number of decimal places: ")
+
+while (roundto >= 'a' and roundto <= 'z'):
+    roundto = input("Enter the NUMBER of decimal places: ")
+
+if not roundto:
+    print("You must enter an integer.")
+    roundto = input("Enter the number of decimal places: ")
+
+if (int(roundto) < 1 or int(roundto) > 100):
+    print("Too far! Choose a number between 0 and 100.")
+    roundto = input("Enter a reasonable number of decimal places: ")
+
 roundint = int(roundto)
 
-roundval = round(math.pi, roundint)
-
-print(roundval)
+print(calculate(roundint))
